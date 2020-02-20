0a82b7ef7252cdf090521373e62870d62cb02190
zhuris <g.zhuris@gmail.com>
2020-02-20 17:52:02 +0200

    adaptive mobile

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ $(document).ready(function(){
     //slider
 
     $('.main-slider').slick({
-        infinite: false,
         slidesToShow: 4,
         slidesToScroll: 1,
+        infinite: true,
         dots: false,
         prevArrow:"<button type='button' class='slick-prev'><svg id=\"Layer_1\" data-name=\"Layer 1\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 192.5 386.26\"><defs><style>.cls-1{fill:#fb9a50;}</style></defs><title>down-aleft</title><polygon class=\"cls-1\" points=\"192.5 0 0 193.13 192.5 386.26 192.5 0\"/></svg></button>" ,
         nextArrow: "<button type='button' class='slick-next'><svg id=\"Layer_1\" data-name=\"Layer 1\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 192.5 386.26\"><defs><style>.cls-1{fill:#fb9a50;}</style></defs><title>down-right</title><polygon class=\"cls-1\" points=\"0 386.26 192.5 193.13 0 0 0 386.26\"/></svg></button>",
@@ -35,15 +35,20 @@ $(document).ready(function(){
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
                 }
             }
             // You can unslick at a given breakpoint now by adding:
